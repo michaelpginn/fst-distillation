c7ee3d83f8bf04eeaac5d549a00bf3139c0490d6
Michael Ginn <michaelginn00@gmail.com>
2025-10-24 16:59:21 -0600

    Use scratch dir

--- run_sweep.sh
+++ run_sweep.sh
@@ -19,4 +19,4 @@ cd "/projects/$USER/fst-distillation"
 set -x
 
 echo Running for $1 with objective $2
-python -m src.sweep data/inflection $1 --features --objective $2
+python -m src.sweep data/inflection $1 --features --objective $2 --models /scratch/alpine/$USER/fst-distillation/models/
