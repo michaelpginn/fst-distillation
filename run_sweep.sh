91378ed874a638bf6ec25a9d6e718804659007ed
Michael Ginn <migi8081@login-ci4.rc.colorado.edu>
2025-10-25 15:41:15 -0600

    Drop walltime

--- run_sweep.sh
+++ run_sweep.sh
@@ -4,7 +4,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem-per-cpu=4GB
 #SBATCH --cpus-per-task=4
-#SBATCH --time=7-00:00:00
+#SBATCH --time=2-00:00:00
 #SBATCH --qos=blanca-curc-gpu
 #SBATCH --partition=blanca-curc-gpu
 #SBATCH --account=blanca-curc-gpu
