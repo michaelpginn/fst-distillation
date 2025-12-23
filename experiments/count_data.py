9e142c31591a41c9c55cd3c86cd4953a8c62b7a2
Michael Ginn <michaelginn00@gmail.com>
2025-12-23 17:24:08 -0600

    Clear raw

---
+++ experiments/count_data.py
@@ -0,0 +1,79 @@
+"""Count examples for each dataset"""
+
+from src.data.unaligned.example import load_examples_from_file
+from src.paths import create_paths
+
+datasets = {
+    "inflection": [
+        "aka",
+        "ceb",
+        "crh",
+        "czn",
+        "dje",
+        "gaa",
+        "izh",
+        "kon",
+        "lin",
+        "mao",
+        "mlg",
+        "nya",
+        "ood",
+        "orm",
+        "ote",
+        "san",
+        "sot",
+        "swa",
+        "syc",
+        "tgk",
+        "tgl",
+        "xty",
+        "zpv",
+        "zul",
+    ],
+    "g2p": [
+        "ara",
+        "bul",
+        "eng",
+        "fas",
+        "ind",
+        "mkd",
+        "pus",
+        "rus",
+        "spa",
+        "tgl",
+        "ukr",
+        "urd",
+    ],
+    "histnorm": ["deu", "hun", "isl", "por", "slv", "spa", "swe"],
+}
+
+for key in datasets:
+    rows = []
+    for lang in datasets[key]:
+        paths = create_paths(
+            data_folder=f"data/{key}",
+            dataset=lang,
+            has_features=key == "inflection",
+            output_split=key == "g2p",
+            merge_outputs="bpe" if key == "inflection" else "right",
+            models_folder=None,
+        )
+        rows.append(
+            [lang, ""]
+            + [
+                format(
+                    len(
+                        load_examples_from_file(
+                            paths[k],
+                            paths["has_features"],
+                            paths["output_split_into_chars"],
+                        )
+                    ),
+                    ",",
+                )
+                for k in ["train", "eval", "test"]
+            ]
+        )
+    for row in rows:
+        print(" & ".join(row) + " \\\\")
+    print("\n\n")
