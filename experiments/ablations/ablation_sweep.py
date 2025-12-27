6dfc93799fe6f4f47e94c91cb5db682936b027d1
Michael Ginn <michaelginn00@gmail.com>
2025-12-27 00:12:52 -0600

    Add more viz stuff and ablations

---
+++ experiments/ablations/ablation_sweep.py
@@ -0,0 +1,107 @@
+import logging
+
+import numpy as np
+
+import wandb
+from src.extract_fst import ExtractionHyperparameters, compute_activations, extract_fst
+from src.paths import create_arg_parser, create_paths_from_args
+
+parser = create_arg_parser()
+parser.add_argument("ablation_name")
+parser.add_argument("model_id")
+parser.add_argument(
+    "--mode",
+    choices=["sample", "search"],
+    help="If sample, will train an alignment predictor to sample from the domain. If search, will use n-grams with BFS to collect activations",
+    required=True,
+)
+parser.add_argument("--no-full-domain", action="store_true")
+args = parser.parse_args()
+paths = create_paths_from_args(args)
+
+num_extract_runs = 100
+logger = logging.getLogger(__name__)
+
+activations, transition_labels = compute_activations(
+    ExtractionHyperparameters(
+        model_shortname=args.model_id,  # type:ignore
+        clustering_method="kmeans",
+        kmeans_num_clusters=1,
+        n_components=None,
+        full_domain=not args.no_full_domain,
+        full_domain_mode=args.mode,
+        full_domain_search_n=3,
+    ),
+    paths,
+)
+max_clusters = len(np.unique(activations, axis=0))
+
+sweep_configuration = {
+    "name": paths["identifier"],
+    "method": "bayes",
+    "metric": {"goal": "maximize", "name": "eval.f1"},
+    "parameters": {
+        "state_split_classifier": {"values": ["svm", "logistic"]},
+        "minimum_transition_count": {
+            "values": [None, 2, 3, 4, 5, 10, 15, 20, 25, 30, 40, 50]
+        },
+        "kmeans_num_clusters": {"min": 50, "max": max_clusters},
+        **(
+            {"full_domain_search_n": {"values": [3, 4]}}
+            if args.mode == "search"
+            else {}
+        ),
+    },
+}
+
+
+def _run_extraction():
+    with wandb.init(
+        entity="lecs-general",
+        project=f"fst-distillation.extraction.v2.{args.ablation_name}",
+        config={
+            "rnn": {
+                # "eval.loss": best_run_loss,
+                "name": args.model_id,  # type:ignore
+            },
+            "identifier": paths["identifier"],
+        },
+        # dir=args.wandb_dir,
+    ) as run:
+        full_domain_search_n = run.config.get("full_domain_search_n", 3)
+        hyperparams = ExtractionHyperparameters(
+            model_shortname=args.model_id,  # type:ignore
+            clustering_method="kmeans",
+            use_faiss=False,
+            state_split_classifier=run.config["state_split_classifier"],
+            n_components=None,
+            minimum_transition_count=run.config["minimum_transition_count"],
+            kmeans_num_clusters=run.config["kmeans_num_clusters"],
+            full_domain=not args.no_full_domain,
+            full_domain_mode=args.mode,
+            full_domain_search_n=full_domain_search_n,
+        )
+        results, _ = extract_fst(
+            hparams=hyperparams,
+            paths=paths,
+            precomputed_activations=(activations, transition_labels)  # type:ignore
+            if full_domain_search_n == 3
+            else None,
+        )
+        run.log(results)
+        # run.summary["training_run"] = best_run.url  # type:ignore
+
+
+logger.info("Creating new sweep")
+fst_sweep_id = wandb.sweep(
+    sweep=sweep_configuration,
+    entity="lecs-general",
+    project=f"fst-distillation.extraction.v2.{args.ablation_name}",
+)
+
+wandb.agent(fst_sweep_id, function=_run_extraction, count=num_extract_runs)
+sweep = wandb.Api().sweep(
+    f"lecs-general/fst-distillation.extraction.v2.{args.ablation_name}/sweeps/{fst_sweep_id}"
+)
+best_run = sweep.best_run()
+print(f"Best run: {best_run.url}")
--- experiments/create_viz.py
+++ experiments/create_viz.py
@@ -4,12 +4,14 @@ from pathlib import Path
 import pandas as pd
 
 import wandb
+from src.data.unaligned.example import load_examples_from_file
+from src.paths import create_paths
 
 parser = argparse.ArgumentParser()
 parser.add_argument("task", choices=["inflection", "g2p", "histnorm"])
 args = parser.parse_args()
 
-viz_path = Path(__file__).parent
+viz_path = Path(__file__).parent / "viz"
 if args.task == "inflection":
     beemer_results = pd.read_csv(viz_path / "beemer.csv")
     beemer_results.index = ["Human Expert"]
@@ -33,6 +35,7 @@ for run in api.runs(path="lecs-general/fst-distillation.ostia"):
     else:
         raise ValueError()
 
+
 # Ours
 sweeps = api.project(
     name="fst-distillation.extraction.v2", entity="lecs-general"
@@ -41,7 +44,9 @@ our_results = {}
 for sweep in sweeps:
     best_run = sweep.best_run()
     assert best_run
-    lang = sweep.name.split(".")[-1]
+    task, lang = sweep.name.split(".")
+    if task != args.task:
+        continue
     our_results[lang] = best_run.summary_metrics["test"]["f1"]
 
 results = pd.DataFrame(
@@ -51,6 +56,28 @@ if beemer_results is not None:
     results = pd.concat([beemer_results, results])
 
 
+if args.task == "histnorm":
+    nochange_results = {}
+    for lang in our_results:
+        paths = create_paths(
+            data_folder="data/histnorm",
+            dataset=lang,
+            has_features=False,
+            output_split=False,
+            merge_outputs="right",
+            models_folder=None,
+        )
+        test_examples = load_examples_from_file(
+            paths["test"], paths["has_features"], paths["output_split_into_chars"]
+        )
+        nochange_results[lang] = len(
+            [ex for ex in test_examples if ex.input_string == ex.output_string]
+        ) / len(test_examples)
+    results = pd.concat(
+        [pd.DataFrame([nochange_results], index=["No Change"]), results]
+    )
+
+
 # Create LaTeX
 def fmt(x):
     if pd.isna(x):
@@ -58,23 +85,41 @@ def fmt(x):
     return f"{x:.3f}"
 
 
-with open(viz_path / "main.tex", "w") as f:
+with open(viz_path / f"{args.task}.tex", "w") as f:
+    fields = ["\\rot{OSTIA}", "\\rot{OSTIA-DD}", "\\rot{Ours}"]
+    if args.task == "inflection":
+        fields = ["\\rot{Human Expert}"] + fields
+    if args.task == "histnorm":
+        fields = ["\\rot{No Change}"] + fields
     f.write(
         "\\begin{table}[htb]\n"
         "\\begin{tabularx}{\\columnwidth}{l | X | X X X }\n"
-        "& \\rot{Human Expert} & \\rot{OSTIA} & \\rot{OSTIA-DD} & \\rot{Ours} \\\\ \n"
-        "\\hline \n"
+        "\\toprule"
+        f"& {' & '.join(f for f in fields)} \\\\ \n"
+        "\\midrule \n"
     )
     for row in results.T.iterrows():
         lang = row[0]
-        beemer = fmt(row[1]["Human Expert"])
-        ostia = fmt(row[1]["OSTIA"])
-        ostia_dd = fmt(row[1]["OSTIA-DD"])
-        ours = "\\textbf{" + fmt(row[1]["Ours"]) + "}"
-        f.write(f"{lang} & {beemer} & {ostia} & {ostia_dd} & {ours} \\\\\n")
+        scores = []
+        if "Human Expert" in row[1]:
+            scores.append(fmt(row[1]["Human Expert"]))
+        if "No Change" in row[1]:
+            scores.append(fmt(row[1]["No Change"]))
+        scores.append(fmt(row[1]["OSTIA"]))
+        scores.append(fmt(row[1]["OSTIA-DD"]))
+        scores.append("\\textbf{" + fmt(row[1]["Ours"]) + "}")
+        scores_str = " & ".join(scores)
+        f.write(f"{lang} & {scores_str} \\\\\n")
+    if args.task == "inflection":
+        task_name = "morphological inflection"
+    elif args.task == "g2p":
+        task_name = "grapheme-to-phoneme"
+    else:
+        task_name = "historical normalization"
     f.write(
+        "\\bottomrule"
         "\\end{tabularx}\n"
-        "\\caption{Accuracy of learned transducers for morphological inflection datasets on held-out test set. Our system produces far more accurate transducers than the OSTIA-based algorithms on every language, and often nearly as accurate as expert-crafted FSTs from \\citet{beemer-etal-2020-linguist}.}\n"
-        "\\label{tab:inflection}\n"
+        f"\\caption{{Accuracy of learned transducers for {task_name} datasets on held-out test set.}}\n"
+        f"\\label{{tab:{args.task}}}\n"
         "\\end{table}"
     )
