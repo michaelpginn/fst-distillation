64340aa21877ba39fb4e01b793088145c2c786ea
Michael Ginn <michaelginn00@gmail.com>
2025-12-28 12:56:22 -0600

    Fix ostia for spa

--- src/ostia/run_ostia.py
+++ src/ostia/run_ostia.py
@@ -1,5 +1,6 @@
 import os
 import pprint
+import sys
 from logging import getLogger
 from typing import Literal
 
@@ -13,6 +14,7 @@ from ..evaluate import evaluate_all
 from ..paths import Paths, create_arg_parser, create_paths_from_args
 
 logger = getLogger(__name__)
+sys.setrecursionlimit(10000)
 
 
 def run_ostia(paths: Paths, order: Literal["lex", "dd"]):
@@ -42,10 +44,12 @@ def run_ostia(paths: Paths, order: Literal["lex", "dd"]):
         samples.append((input_string, ex.output_string))
 
     fst = ostia(samples, order, use_cython=True)
+    fst.save(f"{paths['identifier']}.fst")
+    logger.info(f"Created FST with {len(fst.states)} states")
     fst = FST.re(".* '<sink>':'#'") @ fst
 
-    train_metrics = evaluate_all(fst, train_examples, output_raw_string=True)
-    logger.info(f"Train metrics: {pprint.pformat(train_metrics)}")
+    # train_metrics = evaluate_all(fst, train_examples, output_raw_string=True)
+    # logger.info(f"Train metrics: {pprint.pformat(train_metrics)}")
     eval_metrics = evaluate_all(fst, eval_examples, output_raw_string=True, log=True)
     logger.info(f"Eval metrics: {pprint.pformat(eval_metrics)}")
     test_metrics = evaluate_all(fst, test_examples, output_raw_string=True)
